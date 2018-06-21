970617efec30ece26892357546a54de6eb64d30d
Valentin Volkl <valentin.volkl@cern.ch>
2018-06-21 16:29:35 +0200

    Add tests for createGraph utility

--- tests/test_cmgraph_class.cpp
+++ tests/test_cmgraph_class.cpp
@@ -21,3 +21,19 @@ TEST_CASE("CM Graph methods", "[interface]") {
   auto g = CMGraph();
   g.theLayers = std::vector<CMLayer>{l1, l2};
 }
+
+TEST_CASE("CM Graph User construction", "[interface]") {
+
+  std::vector<std::vector<std::string>> layerDescription { {{"b1","b2","b3", "b4"}, {"b1","b2","b3", "e1"}, {"e1", "e2", "e3", "e4"}, {"b1", "b2", "e1", "e2"}, {"b1", "e1", "e2", "e3"} }};
+  auto g = createGraph(layerDescription);
+
+  REQUIRE(g.theLayers[g.theRootLayers[0]] == "b1");
+
+  REQUIRE(g.theLayers[g.theRootLayers[1]] == "e1");
+
+  REQUIRE(g.theLayers[g.theLayers[0].theOuterLayers[0]] == "b2");
+
+  REQUIRE(g.theLayers[g.theLayers[0].theOuterLayers[1]] == "e1");
+
+
+}
