b07b2a24bff3613d7d271124f4acf6b1b0c791a4
Valentin Volkl <valentin.volkl@cern.ch>
2018-06-28 00:19:06 +0200

    Clean up

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
-git submodule init
-git submodule update
 mkdir build install
-git apply --stat python2.7.patch
+git submodule add https://github.com/pybind/pybind11 python/pybind11
+patch -p1 < python2.7.patch 
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install
 make install -j 4
 cd ..
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/install/lib/:$PWD/install/lib64
-export PYTHONPATH=$PYTHONPATH:$PWD/install/lib/:$PWD/install/lib64
 
 
 
--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 #on lxplus / machines with cvmfs
 source /cvmfs/fcc.cern.ch/sw/views/releases/0.9.1/x86_64-slc6-gcc62-opt/setup.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/install/lib/:$PWD/install/lib64
+export PYTHONPATH=$PYTHONPATH:$PWD/install/lib/:$PWD/install/lib64
