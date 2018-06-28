dd310269f860b3d21a37145f422519dcdbaa5762
Valentin Volkl <valentin.volkl@cern.ch>
2018-06-28 12:32:44 +0200

    make build script usable for travis

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-mkdir build install
-git submodule add https://github.com/pybind/pybind11 python/pybind11
-patch -p1 -t < python2.7.patch 
+mkdir build install || true
+git submodule add https://github.com/pybind/pybind11 python/pybind11 || true
+patch -p1 -t < python2.7.patch || true
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install -Dtricktrack_logger=ON -Dtricktrack_logger_standalone=ON -Dtricktrack_python=ON
 make install -j 4
+make test
 cd ..
 
 
