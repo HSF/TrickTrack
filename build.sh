5266056e76f9b55d24784eda1c35bcb5aa39607b
Valentin Volkl <valentin.volkl@cern.ch>
2018-06-28 11:04:40 +0200

    Add cmake options for python

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir build install
 git submodule add https://github.com/pybind/pybind11 python/pybind11
-patch -p1 < python2.7.patch 
+patch -p1 -t < python2.7.patch 
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../install
+cmake .. -DCMAKE_INSTALL_PREFIX=../install -Dtricktrack_logger=ON -Dtricktrack_logger_standalone=ON -Dtricktrack_python=ON
 make install -j 4
 cd ..
 
