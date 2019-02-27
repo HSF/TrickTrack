e710930d3bad70ebaf5f38106f10f6649967725c
Valentin Volkl <valentin.volkl@cern.ch>
2019-02-27 16:03:05 +0100

    move pybind patch

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir build install integration || true
 git submodule add https://github.com/pybind/pybind11 python/pybind11 || true
-patch -p1 -t < python2.7.patch || true
+patch -p1 -t < python/python2.7.patch || true
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install -Dtricktrack_logger=ON -Dtricktrack_logger_standalone=ON -Dtricktrack_python=ON
 make install -j 4
