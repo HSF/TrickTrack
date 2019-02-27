196e059b5f70c27af78d4911ecf8740168c570fa
Valentin Volkl <valentin.volkl@cern.ch>
2019-02-27 18:30:41 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 mkdir build install integration || true
 git submodule add https://github.com/pybind/pybind11 python/pybind11 || true
-patch -p1 -t < python/python2.7.patch || true
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../install -Dtricktrack_logger=ON -Dtricktrack_logger_standalone=ON -Dtricktrack_python=ON
+cd python
+patch -p1 -t < python2.7.patch || true
+cd ../build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install  -Dtricktrack_python=ON
 make install -j 4
 make test
 cd ..
